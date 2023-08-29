d64e753c9bb2f6d50074bfd17839160a4323ad79
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-08-29 22:24:21 +0200

    removed some console.logs

--- src/functions/CheckRound.jsx
+++ src/functions/CheckRound.jsx
@@ -2,34 +2,22 @@ import CheckDate from "./CheckDate";
 import UpdateRound from "../API/UpdateRound";
 import { RoundIsOpen, RoundIsActive } from "./Round";
 
-export default async function CheckRound(data) {
-  // console.log("Checkround incoming data:", data);
-  const startDateIsPassed = CheckDate(data.startDate);
-  // console.log("startdate passed:", startDateIsPassed);
-  const endDateIsPassed = CheckDate(data.endDate);
-  // console.log("enddate passed:", endDateIsPassed);
-  if (startDateIsPassed && data.isOpen) {
-    // console.log("Incoming data:", data);
-    const updatedRound = RoundIsOpen(data, false);
-    // console.log("Updated round:", updatedRound);
-    const result = await UpdateRound(updatedRound);
-    // console.log("Updated data:", result);
+export default async function CheckRound(round) {
+  const startDateIsPassed = CheckDate(round.startDate);
+  const endDateIsPassed = CheckDate(round.endDate);
+  if (startDateIsPassed && round.isOpen) {
+    const updatedRound = RoundIsOpen(round, false);
+    await UpdateRound(updatedRound);
 
     return true;
-  } else if (startDateIsPassed === false && data.isOpen === false) {
-    console.log("Incoming data:", data);
-    const updatedRound = RoundIsOpen(data, true);
-    console.log("Updated round:", updatedRound);
-    const result = await UpdateRound(updatedRound);
-    console.log("Updated data:", result);
+  } else if (startDateIsPassed === false && round.isOpen === false) {
+    const updatedRound = RoundIsOpen(round, true);
+    await UpdateRound(updatedRound);
   }
 
-  if (endDateIsPassed && data.isActive) {
-    // console.log("Incoming data:", data);
-    const updatedRound = RoundIsActive(data, false);
-    // console.log("Updated round:", updatedRound);
-    const result = await UpdateRound(updatedRound);
-    // console.log("Updated data:", result);
+  if (endDateIsPassed && round.isActive) {
+    const updatedRound = RoundIsActive(round, false);
+    await UpdateRound(updatedRound);
 
     return true;
   }
