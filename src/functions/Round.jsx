031dc8c6eedbce256a5b53fc6f86ac226184c384
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-08-29 22:24:48 +0200

    added comment

--- src/functions/Round.jsx
+++ src/functions/Round.jsx
@@ -1,5 +1,4 @@
 export function RoundIsOpen(round, updateIsOpen) {
-  console.log("Inside round component:", round);
   const data = {
     Id: round.id,
     Title: round.title,
@@ -7,23 +6,20 @@ export function RoundIsOpen(round, updateIsOpen) {
     EndDate: round.endDate,
     UserId: round.userId,
     IsActive: round.isActive,
-    IsOpen: updateIsOpen,
+    IsOpen: updateIsOpen, //This is what will update
   };
-  console.log("Data leaving round component:", data);
   return data;
 }
 
 export function RoundIsActive(round, updateIsActive) {
-  console.log("Inside round component:", round);
   const data = {
     Id: round.id,
     Title: round.title,
     StartDate: round.startDate,
     EndDate: round.endDate,
     UserId: round.userId,
-    IsActive: updateIsActive,
+    IsActive: updateIsActive, //This is what will update
     IsOpen: round.isOpen,
   };
-  console.log("Data leaving round component:", data);
   return data;
 }
