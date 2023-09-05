6cc42d76ee3a10c3f85ebf8c336ee8afc1db1f8b
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-09-05 15:27:41 +0200

    no need, api handles it

--- src/functions/StryktipsetInfoToCouponArray.jsx
+++
@@ -1,7 +0,0 @@
-export default function StryktipsetInfoToCouponArray(couponWithInfo) {
-  const onlyCoupon = [];
-  for (let i = 0; i < couponWithInfo.Array.length; i++) {
-    onlyCoupon.push(couponWithInfo[i].outcome);
-  }
-  return onlyCoupon;
-}
