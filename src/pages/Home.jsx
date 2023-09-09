db5b800df56e56f0d497066122809d0f356ed280
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-09-09 23:43:35 +0200

    placeholder text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import "./Home.css";
 
-import HomeTopContent from "../components/HomeTopContent";
 import SiteInfo from "../components/SiteInfo";
 
 import { Outlet } from "react-router-dom";
@@ -9,7 +8,15 @@ export default function Home() {
   return (
     <section>
       <article>
-        <HomeTopContent />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, a
+          esse amet eaque perferendis repellat? Quae accusantium nostrum
+          voluptatem numquam quod. Explicabo aliquid commodi, est aliquam soluta
+          nemo corporis necessitatibus dolorem quo non veritatis a quia incidunt
+          quos corrupti repudiandae! Porro consequuntur aliquam consectetur
+          numquam iste recusandae dolore. Repellendus, saepe?
+        </p>
+        {/* <HomeTopContent /> */}
         <Outlet />
         <SiteInfo />
       </article>
