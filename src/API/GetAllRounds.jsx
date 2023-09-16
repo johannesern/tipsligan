47f284525c2d3dec84f6d25d36b98cc502755057
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-09-16 21:52:43 +0200

    new api call

---
+++ src/API/GetAllRounds.jsx
@@ -0,0 +1,20 @@
+import { DevURL } from "../constants/Constants";
+import { TipsliganAPIURL } from "../constants/Constants";
+
+export default async function GetAllRounds() {
+  const baseUrl = DevURL();
+  try {
+    const response = await fetch(baseUrl + "/rounds");
+    // console.log("User response:", response);
+
+    if (response.ok) {
+      const responseData = await response.json();
+      //   console.log("Fetchuser response:", responseData);
+      return responseData;
+    } else {
+      console.error("Failed to get data");
+    }
+  } catch (error) {
+    console.error("API:GetAllUsers error", error);
+  }
+}
