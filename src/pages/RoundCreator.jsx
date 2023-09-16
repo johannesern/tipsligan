95bd50d12efbc9c05015bf5025c5f78bd691c1eb
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-09-16 21:40:21 +0200

    roundcreator done

--- src/pages/RoundCreator.jsx
+++ src/pages/RoundCreator.jsx
@@ -1,17 +1,25 @@
-import { useEffect, useState } from "react";
-import StartDatepicker from "../components/DatePicker/StartDatePicker.jsx";
-import { CreateRound } from "../API/CreateRound.jsx";
-import { FormattedDate } from "../functions/FormattedDate.jsx";
-import "./RoundCreator.css";
+import { useEffect, useState } from 'react';
+import StartDatepicker from '../components/DatePicker/StartDatePicker.jsx';
+import { CreateRound } from '../API/CreateRound.jsx';
+import { FormattedDate } from '../functions/FormattedDate.jsx';
+import './RoundCreator.css';
 
 export default function RoundCreator() {
+  const defaultStartDate = FormattedDate();
+  const defaultPeriodInWeeks = '10';
+  const [createdRound, setCreatedRound] = useState();
   const [roundData, setRoundData] = useState({
-    title: "",
-    periodInWeeks: "",
-    startDate: "",
-    isOpen: true,
-    isActive: true,
+    title: '',
+    periodInWeeks: defaultPeriodInWeeks,
+    startDate: defaultStartDate,
+    isOpen: 'true',
+    isActive: 'true',
   });
+
+  const activeRoundAlreadyExists =
+    'En aktiv runda finns redan. Avvaktivera befintlig runda innan ny runda aktiveras.';
+  const newRoundCreated = 'Ny runda skapad!';
+
   const getStartDate = (childStartDate) => {
     setRoundData({
       ...roundData,
@@ -32,28 +40,51 @@ export default function RoundCreator() {
   );
 
   useEffect(() => {
-    console.log("Round data:", roundData);
+    // console.log('Round data:', roundData);
   }, [roundData]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Send data:", roundData);
-    const response = await CreateRound(roundData);
-    console.log("Createround response:", response);
+
+    // js is stupid... needs to convert string to bool before POST because
+    // selects change a bool-value to string on the fly... stupid
+    const modifiedRoundData = {
+      ...roundData,
+      isActive:
+        roundData.isActive === 'true'
+          ? true
+          : roundData.isActive === 'false'
+          ? false
+          : null,
+      isOpen:
+        roundData.isOpen === 'true'
+          ? true
+          : roundData.isOpen === 'false'
+          ? false
+          : null,
+    };
+
+    const createdRound = await CreateRound(modifiedRoundData);
+    displayRoundCreated(createdRound);
   };
+
+  const displayRoundCreated = (round) => {
+    setCreatedRound(round);
+  };
+
   return (
     <>
-      <div className="">
+      <div className=''>
         <h2>Skapa ny omgång</h2>
-        <form className="create-new-round" onSubmit={handleSubmit}>
-          <div className="title-weeks">
+        <form className='create-new-round' onSubmit={handleSubmit}>
+          <div className='title-weeks'>
             <label>
               Titel:
               <input
-                className="input-field"
+                className='input-field'
                 value={roundData.title}
-                type="text"
-                name="title"
+                type='text'
+                name='title'
                 onChange={handleChange}
               />
             </label>
@@ -61,8 +92,9 @@ export default function RoundCreator() {
               Antal veckor:
               <select
                 onChange={handleChange}
-                className="input-field"
-                name="periodInWeeks"
+                defaultValue={10}
+                className='input-field'
+                name='periodInWeeks'
               >
                 {weeks.map((week, index) => (
                   <option key={index} value={week}>
@@ -74,25 +106,25 @@ export default function RoundCreator() {
             <label>
               Öppen för registrering:
               <select
-                className="input-field"
-                name="isOpen"
+                className='input-field'
+                name='isOpen'
                 onChange={handleChange}
                 defaultValue={true}
               >
-                <option value={true}>Ja</option>
-                <option value={false}>Nej</option>
+                <option value='true'>Ja</option>
+                <option value='false'>Nej</option>
               </select>
             </label>
             <label>
               Rundan aktiv:
               <select
-                className="input-field"
-                name="isActive"
+                className='input-field'
+                name='isActive'
                 onChange={handleChange}
                 defaultValue={true}
               >
-                <option value={true}>Ja</option>
-                <option value={false}>Nej</option>
+                <option value='true'>Ja</option>
+                <option value='false'>Nej</option>
               </select>
             </label>
           </div>
@@ -103,10 +135,21 @@ export default function RoundCreator() {
           </label>
           <br />
           <br />
-          <button onSubmit={handleSubmit} type="submit">
+          <button onSubmit={handleSubmit} type='submit'>
             Skapa omgång
           </button>
         </form>
+        {createdRound ? (
+          <>
+            {createdRound.isActive != false ? (
+              <h3 className=''>{newRoundCreated}</h3>
+            ) : (
+              <h3 className=''>{activeRoundAlreadyExists}</h3>
+            )}
+          </>
+        ) : (
+          <></>
+        )}
       </div>
     </>
   );
