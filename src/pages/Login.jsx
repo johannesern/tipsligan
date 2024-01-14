2f45b6c507e8a447343b9471f1da1bc02da19f11
Johannes Ragnarsson <johannes.ragnarsson@fasttrack-solutions.com>
2024-01-14 21:11:27 +0100

    refactor: changes for controlflow in login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,7 +10,8 @@ export default function Login() {
   const [email, setUserName] = useState();
   const [password, setPassword] = useState();
   const [loginError, setLoginError] = useState();
-  const [isUser, setIsUser] = useState(true);
+  const [isUser, setIsUser] = useState(false);
+  const [choiceMade, setChoiceMade] = useState(false);
 
   //Store
   const adminTokenInStore = useStore((state) => state.adminToken);
@@ -66,58 +67,105 @@ export default function Login() {
 
   return (
     <div className="login-wrapper">
-      <h1>Logga in som</h1>
+      <h1>Logga in</h1>
       <br />
-      <div className="login_user-type">
-        <button
-          type="button"
-          disabled={isUser}
-          className={isUser ? "" : "login_inactive"}
-          onClick={() => setIsUser(true)}
-        >
-          Deltagare
-        </button>
-        <button
-          type="button"
-          disabled={!isUser}
-          className={isUser ? "login_inactive" : ""}
-          onClick={() => setIsUser(false)}
-        >
-          Administratör
-        </button>
-      </div>
+      {!choiceMade && (
+        <>
+          <div>
+            <i>Markera en användare</i>
+          </div>
+          <br />
+          <div className="login_user-type">
+            <button
+              type="button"
+              className="login_inactive"
+              onClick={() => {
+                setIsUser(true), setChoiceMade(true);
+              }}
+            >
+              Deltagare
+            </button>
+            <button
+              type="button"
+              className="login_inactive"
+              onClick={() => {
+                setIsUser(false), setChoiceMade(true);
+              }}
+            >
+              Administratör
+            </button>
+          </div>
+        </>
+      )}
+      {choiceMade && (
+        <>
+          <div>
+            <i>Markera en användare</i>
+          </div>
+          <br />
+          <div className="login_user-type">
+            <button
+              type="button"
+              disabled={isUser}
+              className={isUser ? "" : "login_inactive"}
+              onClick={() => setIsUser(true)}
+            >
+              Deltagare
+            </button>
+            <button
+              type="button"
+              disabled={!isUser}
+              className={isUser ? "login_inactive" : ""}
+              onClick={() => setIsUser(false)}
+            >
+              Administratör
+            </button>
+          </div>
+        </>
+      )}
       <br />
       <form onSubmit={handleSubmit}>
         <table>
           <tbody>
             <tr>
-              <td>
+              <td className="login_label-inputfield">
                 <label>Email: </label>
-              </td>
-              <td>
                 <input
                   className="login_input-field"
                   type="text"
+                  placeholder="Skriv in email..."
+                  disabled={!choiceMade}
                   onChange={(e) => setUserName(e.target.value)}
                 />
               </td>
             </tr>
             <tr>
-              <td>
+              <td className="login_label-inputfield">
                 <label>Lösenord:</label>
-              </td>
-              <td>
                 <input
                   type="password"
+                  placeholder="Skriv in lösenord..."
+                  disabled={!choiceMade}
                   className="login_input-field"
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </td>
             </tr>
+            <tr>
+              <td onClick={() => navigate("/återställa-lösenord")}>
+                <a href="">Glömt lösenord?</a>
+              </td>
+            </tr>
           </tbody>
         </table>
-        <div>
-          <button type="submit">Logga in</button>
+        <div className="login_align-login-btn">
+          <button
+            disabled={!choiceMade}
+            className={choiceMade ? "" : "login_inactive-btn"}
+            type="submit"
+          >
+            Logga in
+          </button>
           {loginError && <p>{loginError}</p>}
         </div>
       </form>
